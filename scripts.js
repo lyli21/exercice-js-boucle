6987645e2e124daf558ce1bfb3399972f0276010
lyli21 <pro.lylia@gmail.com>
2024-11-05 22:12:52 +0100

    exerices des 3 énoncés

--- scripts.js
+++ scripts.js
@@ -5,19 +5,33 @@
 // while (prenom != "") {
 //     prenom = prompt("saisissez prenom " + i)
 //     i++;
-  
+
 //     console.log(prenom);
 // }
 
 // console.log(i + -2)
-²
+
+
 /////////////////////// ENTIERS  Inferieur //////////////////////////////
 
 
-let N = ""(prompt("Entré la valeur de N"));
-N = parseInt;
 
-for (let i =0; i <10; i--); {
-    
-}
-console.log(N);
+
+// let N = 10; 
+
+// for (let i = 0; i >= -10; i--) { // jai passer bcp de temps parce que jai pas mis un - à ma variable N dans mon instruction et aussi pour safficher jusqua 10 c'est inferieur egal à -10//
+//     console.log(i);
+// }
+
+
+
+//Somme des entiers inférieurs à N//
+
+let N = 10; 
+let somme= 0;
+
+for (let i = 0; i >= -10; i--) {
+    console.log(i);
+    somme +=i;
+    }
+    console.log("la somme des nombres inférieur à " + -N + "est" + somme);
