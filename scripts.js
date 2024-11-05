0848b5d926ecf61e39eab0842eec36fb22ab1429
Lylia <pro.lylia@gmail.com>
2024-11-05 17:26:54 +0100

    exerice des boucles

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// let i = 1;
+// let prenom;
+// let nprenom;
+
+// while (prenom != "") {
+//     prenom = prompt("saisissez prenom " + i)
+//     i++;
+  
+//     console.log(prenom);
+// }
+
+// console.log(i + -2)
+²
+/////////////////////// ENTIERS  Inferieur //////////////////////////////
+
+
+let N = ""(prompt("Entré la valeur de N"));
+N = parseInt;
+
+for (let i =0; i <10; i--); {
+    
+}
+console.log(N);
